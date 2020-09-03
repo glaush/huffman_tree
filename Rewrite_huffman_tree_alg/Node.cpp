5640eed8d7eebc012609f168bd3f2b81f5bda99d
unknown <lessoctominds@gmail.com>
2020-09-03 13:28:41 +0300

    Added a return value for get_count(), get_value() functions.

--- Rewrite_huffman_tree_alg/Node.cpp
+++ Rewrite_huffman_tree_alg/Node.cpp
@@ -24,13 +24,13 @@ inline Node<T>::Node(const int, const shared_ptr<Node>&, const shared_ptr<Node>&
 template<typename T>
 inline int Node<T>::get_count() const
 {
-    return 0;
+    return count;
 }
 
 template<typename T>
 inline T Node<T>::get_value() const
 {
-    return T();
+    return value;
 }
 
 template<typename T>
