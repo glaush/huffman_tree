b4fcb30471896e51f9120c52742bdc2340771c60
unknown <lessoctominds@gmail.com>
2020-09-03 16:21:08 +0300

    Added a method create_binary_tree for HuffmanTree

--- Rewrite_huffman_tree_alg/HuffmanTree.cpp
+++ Rewrite_huffman_tree_alg/HuffmanTree.cpp
@@ -9,10 +9,30 @@ void HuffmanTree::init(const string& data)
 {
     auto symbols_dictionary = create_dictionary(data);
     auto symbols_vect = move_to_vector(symbols_dictionary);
+
+    create_binary_tree(symbols_vect);
+
 }
 
-void HuffmanTree::create_binary_tree(vector<SymbolPtr>&)
+void HuffmanTree::create_binary_tree(vector<SymbolPtr>& vec)
 {
+    auto symbol_comparator = [](const SymbolPtr a, const SymbolPtr b)
+    {
+        return a->get_count() < b->get_count();
+    };
+
+    while (vec.size() > 1)
+    {
+        std::sort(vec.begin(), vec.end(), symbol_comparator);
+
+        auto left = vec.front();
+        auto right = (*++vec.begin());
+        auto node = new Node<char>(left->get_count() + right->get_count(), left, right);
+    
+        vec.erase(vec.begin(), vec.begin() + 2);
+        vec.push_back(SymbolPtr(node));
+    }
+    binary_tree = vec.front();
 }
 
 map<char, SymbolPtr> HuffmanTree::create_dictionary(const string& data) const
@@ -32,7 +52,13 @@ map<char, SymbolPtr> HuffmanTree::create_dictionary(const string& data) const
     return symbols_dict;
 }
 
-vector<SymbolPtr> HuffmanTree::move_to_vector(map<char, SymbolPtr>&) const
+vector<SymbolPtr> HuffmanTree::move_to_vector(map<char, SymbolPtr>& dictionary) const
 {
-    return vector<SymbolPtr>();
+    vector<SymbolPtr> symbols;
+    symbols.reserve(dictionary.size());
+    for_each(dictionary.begin(), dictionary.end(), [&symbols](pair<const char, SymbolPtr>& symbol)
+    {
+        symbols.push_back(move(symbol.second));
+    });
+    return symbols;
 }
